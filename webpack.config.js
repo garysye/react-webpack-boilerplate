8363d5eb480a60e49bcb851985df9fbced74ceb5
Gary Ye <garysye@gmail.com>
2016-01-20 20:38:54 -0800

    Add boilerplate files with NPM and Webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  }
+};
